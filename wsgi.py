e0e54a750405ff326e55f6858df97e3741ff2541
madennis123 <madennis123@yahoo.co.uk>
2017-09-07 16:58:03 +0100

    modified homepage

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 application = Flask(__name__)
 
+@application.route("/")
+def index():
+    return "<h1> This is the Home Page for Web-app2 </h1>"
+
 @application.route("/profile/<name>")
 def profile(name):
     return render_template("profile.html" , name=name) 	
